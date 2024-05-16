1421cb3630210529eeb0774c466c9771c91eedc6
Thao Ha <thao.ha@asnet.com.vn>
2024-05-16 17:56:58 +0700

    feat: config json

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const jsonServer = require("json-server");
+const jsonServerAuth = require("json-server-auth");
 
+// Create a JSON Server instance
+const server = jsonServer.create();
+
+// Create a router using the 'db.json' file as the data source
+const router = jsonServer.router("db.json");
+
+// Set up default middlewares (logger, static, cors, etc.)
+const middlewares = jsonServer.defaults();
+
+// Use the middlewares
 server.use(middlewares);
+
+// Share the database of the router with jsonServerAuth
+server.db = router.db;
+
+// Use jsonServerAuth for authentication and user management
+server.use(jsonServerAuth);
+
+// Use the router for handling API routes
 server.use(router);
 
-server.listen(port);
+// Start the server on port 3001
+server.listen(3001, () => {
+  // eslint-disable-next-line no-console
+  console.log("JSON Server is running port 3001");
+});
